4e36922c677ed498314c1199d81a594df8664cac
SantiagoBurgos <santiago.burgosc@ug.edu.ec>
2023-06-19 00:28:15 -0500

    Se agrega el menu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,10 @@ const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full'},
   { path: 'avatar', loadChildren: () => import('./pages/avatar/avatar.module').then( m => m.AvatarPageModule)},
   { path: 'botones', loadChildren: () => import('./pages/botones/botones.module').then( m => m.BotonesPageModule)},
+  {
+    path: 'input',
+    loadChildren: () => import('./pages/input/input.module').then( m => m.InputPageModule)
+  },
 ];
 
 @NgModule({
