de2009dd736ab290ded54df8d888115107203552
SantiagoBurgos <santiago.burgosc@ug.edu.ec>
2023-06-19 21:43:03 -0500

    Primer commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full'},
+  { path: 'home', redirectTo: 'home', pathMatch: 'full'},
   { path: 'avatar', loadChildren: () => import('./pages/avatar/avatar.module').then( m => m.AvatarPageModule)},
   { path: 'botones', loadChildren: () => import('./pages/botones/botones.module').then( m => m.BotonesPageModule)},
   {
